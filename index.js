5fdc98c7ba74c7ac9ac0020bf128da3f70ff7c64
Shubham Athawane <Athwale29081999@gmail.com>
2023-05-09 08:32:55 +0530

    Added my project

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 import Routes from "./routes/send.js";
 
 dotenv.config();
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 const Uri = process.env.MONGO_URL;
 
 const connect = () => {
