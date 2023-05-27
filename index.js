5d8e997788ebf477fec2619ab1dd3b360e7daa60
Shubham Athawane <67777638+shubhamathawane@users.noreply.github.com>
2023-05-27 13:03:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ app.use("/api/", Routes);
 // 3: step heroku
 // Deployment 
 
-app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  })
+// app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   })
 
 
 app.listen(port, () => {
